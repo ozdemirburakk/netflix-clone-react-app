1510797fc3b8d5474a74217ea0a24ac43d39f670
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-06-07 16:17:44 +0300

    account page show movies

---
+++ src/components/SavedShows.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
+const SavedShows = () => {
+  const [movies, setMovies] = useState([]);
+  const { user } = UserAuth();
+
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setMovies(doc.data()?.savedShows);
+    });
+  }, [user?.email]);
+
+  const movieRef = doc(db, "users", `${user.email}`);
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedID);
+      await updateDoc(movieRef, {
+        savedShows: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h2 className="p-4 font-bold text-white md:text-xl">My Shows</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="absolute left-0 z-10 hidden bg-white rounded-full opacity-50 cursor-pointer hover:opacity-100 group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider"}
+          className="relative w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
+          {movies.map((item, id) => (
+            <div
+              key={id}
+              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+            >
+              <img
+                className="block w-full h-auto"
+                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className="absolute top-0 left-0 w-full h-full text-white opacity-0 hover:bg-black/80 hover:opacity-100">
+                <p className="flex items-center justify-center h-full text-xs font-bold text-center whitespace-normal md:text-sm">
+                  {item?.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(item.id)}
+                  className="absolute text-gray-300 top-4 right-4"
+                >
+                  <AiOutlineClose />
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="absolute right-0 z-0 hidden bg-white rounded-full opacity-50 cursor-pointer hover:opacity-100 group-hover:block"
+          size={40}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SavedShows;
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <>
+      <div className="w-full text-white">
+      <img
+        className="w-full h-[400px] bject-cover "
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/5ea364b1-8e59-4693-8ad8-f0eaee32d1bf/8521927a-abf7-498a-9bda-de3c0ac9e526/TR-tr-20220530-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt=""
+      />
+      <div className="fixed top-0 left-0 w-full bg-black/60 h-[550px]">
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl font-bold md:text-5xl">My Shows</h1>
+        </div>
+      </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
 
-export default Account
+export default Account;
